c2e31e8ed581faa0361c6076a0a9a672917cce04
LarryZeta <i@larryzeta.cc>
2020-04-21 14:14:49 +0800

    modified:   get_topic_contents.py 	modified:   spader.py

--- get_topic_contents.py
+++ get_topic_contents.py
@@ -6,7 +6,9 @@ import re, csv, os, datetime, requests, random
 
 def get_topic_contents(topic_name, date):
     
-    cookie_str = ''
+    print(date + ': ' + topic_name)
+
+    cookie_str = 'UOR=bbs.aptx.cn,widget.weibo.com,login.sina.com.cn; SINAGLOBAL=7013905332748.457.1584003865013; ULV=1584003866020:1:1:1:7013905332748.457.1584003865013:; SCF=ApvXF8YzDLgwWPzUMd2sb_6uPp2HV_-L96IROT0H59hWaNQG9MyZ0dTkjkoCwrSYqjry_wRKLrzjenXOecQ2ELI.; SUHB=0P1Mp3pOeR_8Vm; ALF=1618826649; un=18810817370; wvr=6; _s_tentry=bbs.aptx.cn; Apache=7013905332748.457.1584003865013; login_sid_t=053dff415dda6dbc469f0df0de88c3fc; cross_origin_proto=SSL; SSOLoginState=1587055794; WBStorage=42212210b087ca50|undefined; SUB=_2A25zmFJJDeRhGeNK41oX8S3EzTuIHXVQ7MSBrDV8PUNbmtCOLWz1kW9NSVUNPhaygEB4b2MVCL0Z9DlK0fDbpl3i; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WFW-LvpH4oXIzuP4wUT1fvR5JpX5KMhUgL.Fo-X1hnceKeRSoM2dJLoI7LeqPiEwHDLUs2t'
     cookies = {}
     for line in cookie_str.split(';'):
         key, value = line.strip().split('=', 1)
@@ -22,14 +24,18 @@ def get_topic_contents(topic_name, date):
     # f = open('a.html', 'r')
     # html = f.read()
     soup = BeautifulSoup(html, features='lxml')
-    li = soup.find('ul', {'class': 's-scroll'})('li')
-    pages_num = len(li) # 热搜页数
-
+    pages_num = 1 # 热搜页数，若没有ul（页数导航条）则为1
+    ul = soup.find('ul', {'class': 's-scroll'})
+    if ul is not None: 
+        pages_num = len(ul('li'))
+    
     file_name = './data/'+ date + '/' + topic_name + '.csv'
     headers = ('user_name', 'text')
     page = 1
     sleep_page = 1
     qbar = tqdm(total=pages_num)
+    sleep(0.1) # 刷新显示
+    qbar.update(1)
 
     with open(file_name, 'a', newline='') as csvFile:
         writer = csv.writer(csvFile)
@@ -47,19 +53,17 @@ def get_topic_contents(topic_name, date):
                     text = feed.find('p', {'class': 'txt', 'node-type': "feed_list_content"}).get_text().replace(' ','').replace('\n', '')
                 row = (user_name, text)
                 writer.writerow(row)
-            
-            page = page + 1
-            qbar.update(1)
 
+            page = page + 1
             if page <= pages_num:
                 if page == sleep_page:
                     sleep(random.randint(6, 10))
                     sleep_page = sleep_page + random.randint(1, 5)
+                sleep(random.randint(1, 3))
                 r = requests.get('https://s.weibo.com/weibo', {'q': topic_name, 'page': page}, cookies = cookies) # 发送下一页请求
+                qbar.update(1)
                 html = r.content.decode('utf-8')
                 soup = BeautifulSoup(html, features='lxml')
-                
-                sleep(random.randint(1, 3))
             else:
                 qbar.close()
                 break
--- spader.py
+++ spader.py
@@ -1,12 +1,14 @@
 import get_topic_contents, csv, random
 from time import sleep
 
-date = '2020-04-16'
-csvFile = open('./data/'+ date +'.csv', 'r')
-reader = csv.reader(csvFile)
+days = []
 
-for item in reader:
-    if reader.line_num == 1:
-        continue
-    get_topic_contents.get_topic_contents(item[0], date)
-    sleep(random.randint(6, 10))
+for date in days:
+    csvFile = open('./data/'+ date +'.csv', 'r')
+    reader = csv.reader(csvFile)
+
+    for item in reader:
+        if reader.line_num == 1:
+            continue
+        get_topic_contents.get_topic_contents(item[0], date)
+        sleep(random.randint(6, 10))
